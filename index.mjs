ff11f49c093fa9fc6a0b0263a60a97c99cb6f5c6
Tobi Olajide <tobiolajide887@yahoo.com>
2023-02-16 17:10:41 +0000

    replaced package.json with updated version for inquirer and updated js to mjs

---
+++ index.mjs
@@ -0,0 +1,25 @@
+import inquirer from 'inquirer';
+import fs from 'inquirer';
+import path from 'path';
+import generateMarkdown from './utils/generateMarkdown';
+// const fs = require("fs");
+// const path = require('path');
+// const inquirer = require("inquirer");
+// const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
