64f4cf3bf3b9a0b470360ca374fb94ee4ff74dcd
Tobi Olajide <tobiolajide887@yahoo.com>
2023-02-18 19:54:40 +0000

    converted html syntax for written file to .md and moved text to generateMarkdown.js

--- index.mjs
+++ index.mjs
@@ -1,11 +1,6 @@
 import inquirer from 'inquirer';
 import fs from 'fs/promises';
-// import path from 'path';
-// import generateMarkdown from './utils/generateMarkdown.js';
-// const fs = require("fs");
-// const path = require('path');
-// const inquirer = require("inquirer");
-// const generateMarkdown = require("./utils/generateMarkdown.js");
+import generateMarkdown from './utils/generateMarkdown.js';
 
 // array of questions for user
 const questions = [
@@ -67,65 +62,11 @@ function writeToFile(fileName, data) {
 init();
 // function to initialize program
 async function init() {
-    const {title, description, installation, usage, contributing, license, tests, question} = await inquirer
+    const response = await inquirer
     .prompt(
-        /* Questions to derive information about the application repo*/
+        // questions to derive information about the application repo
         questions 
     )
-    console.log(`${title}, ${installation}, ${tests}, ${contributing}`)
-    let htmlText = `
-                    <html lang="en">
-                    <head>
-                        <meta charset="UTF-8">
-                        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        <title>Document</title>
-                    </head>
-                    <body>
-                        <main>
-                            <h1>${title}</h1>
-                            <div id = "description">
-                                <h2>Description</h2>
-                                <p>${description}</p>
-                            </div>
-                            <div>
-                                <h2>Table of Contents</h2>
-                                <ul>
-                                    <li><a href="#description">Description</a> </li>
-                                    <li><a href="#installation">Installation</a> </li>
-                                    <li><a href="#usage">Usage</a> </li>
-                                    <li><a href="#license">License</a> </li>
-                                    <li><a href="#contributors">Contributions</a> </li>
-                                    <li><a href="#tests">Tests</a> </li>
-                                    <li><a href="#questions">Questions</a> </li>
-                                </ul>
-                            </div>
-                            <div id = "installation">
-                                <h2>Installation</h2>
-                                <p>${installation}</p>
-                            </div>
-                            <div id = "usage">
-                                <h2>Usage</h2>
-                                <p>${usage}</p>
-                            </div>
-                            <div id = "license">
-                                <h2>License</h2>
-                                <p>${license}</p>
-                            </div>
-                            <div id = "contributors">
-                                <h2>Contributors</h2>
-                                <p>${contributing}</p>
-                            </div>
-                            <div id = "tests">
-                                <h2>Tests</h2>
-                                <p>${tests}</p>
-                            </div>
-                            <div id = "question">
-                                <h2>Questions</h2>
-                                <p>${question}</p>
-                            </div>
-                        </main>
-                    </body>
-                    </html>`
-    writeToFile("index.html", htmlText)
+    // call function to write README file
+    writeToFile("README.md", generateMarkdown(response))
 }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,49 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Description 
+
+${data.description}
+
+## Table of Contents
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage 
+
+### Deployed Application
+
+* The URL of the deployed application:
+[https://${data.usage}](https://${data.usage})
+
+* The URL of the GitHub repository: 
+
+## License
+
+Licensed under the [MIT](https://${data.license}) license
+
+## Contributors
+
+${data.contributing}
+    
+## Tests
+
+${data.tests}
+
+## Questions
+
+${data.question}
+  
 
 `;
 }
